2cdf077f06dd5f3217fa71260a2d7e353b99a4ad
Divesh Kamble <diveshkamble@gmail.com>
2023-10-07 21:21:41 +0530

    Parse User-Agent

--- app/main.py
+++ app/main.py
@@ -2,6 +2,26 @@
 import socket
 
 
+def get_path(req: bytes) -> str:
+    """Extracts path from request"""
+    lines = req.split("\r\n")
+    if lines:
+        path = lines[0].split(" ")
+        if len(path) > 1:
+            return path[1]
+
+    return "/"
+
+
+def get_user_agent(req: bytes) -> str:
+    """Extracts user-agent from request"""
+    for line in req.split("\r\n"):
+        if line.startswith("User-Agent:"):
+            user_agent = line.split(" ", 2)[1]
+            return user_agent
+    return ""
+
+
 def main():
     """Main function"""
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -23,23 +43,19 @@ def main():
             response = b"HTTP/1.1 200 OK\r\n\r\n"
         elif "/echo" in path:
             resp_string = path.split("/", 2)[2]
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(resp_string)}\r\n\r\n{resp_string}".encode()
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(resp_string)}\r\n\r\n{resp_string}".encode(
+                "utf-8"
+            )
+        elif "/user-agent" in path:
+            user_agent = get_user_agent(req)
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode(
+                "utf-8"
+            )
 
         else:
             response = b"HTTP/1.1 404 Not Found\r\n\r\n"
         client.sendall(response)
         client.close()
-
-
-def get_path(req: bytes) -> str:
-    """Extracts path from request"""
-    lines = req.split("\r\n")
-    if lines:
-        path = lines[0].split(" ")
-        if len(path) > 1:
-            return path[1]
-
-    return "/"
 
 
 if __name__ == "__main__":

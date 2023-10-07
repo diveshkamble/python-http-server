c4f53e7131138cbcc0aeb3fd0e4c1de8920a32aa
Divesh Kamble <diveshkamble@gmail.com>
2023-10-07 20:06:16 +0530

    Added logic to parse path

--- app/main.py
+++ app/main.py
@@ -3,18 +3,40 @@ import socket
 
 
 def main():
+    """Main function"""
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
     #
+
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     # server_socket.accept() # wait for client
-    client, addr = server_socket.accept()
 
-    client.recv(1024)
-    response = b"HTTP/1.1 200 OK\r\n\r\n"
-    client.sendall(response)
+    while True:
+        client, _ = server_socket.accept()
+
+        req = client.recv(1024).decode("utf-8")
+        path = get_path(req)
+        print(path)
+        print(path == "/")
+        if path == "/":
+            response = b"HTTP/1.1 200 OK\r\n\r\n"
+        else:
+            response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+        client.sendall(response)
+        client.close()
+
+
+def get_path(req: bytes) -> str:
+    """Extracts path from request"""
+    lines = req.split("\r\n")
+    if lines:
+        path = lines[0].split(" ")
+        if len(path) > 1:
+            return path[1]
+
+    return "/"
 
 
 if __name__ == "__main__":
